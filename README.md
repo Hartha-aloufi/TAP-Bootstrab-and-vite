7ecbf4ba2ea158c6a2f20345da9c0f9ba9fb946f
Hartha Aloufi <user.email>
2024-07-07 14:04:05 +0300

    first commit

---
+++ README.md
@@ -0,0 +1,5 @@
+# TAP-Bootstrab-and-vite
+When: Week 5 Bootstrap session
+
+A basic setup for bootstrap and vite project.
+
---
+++ src/js/main.js
@@ -0,0 +1,6 @@
+import '../css/style.scss'
+
+// Import all of Bootstrap's JS
+// import { Alert } from 'bootstrap'
+import Alert from 'bootstrap/scss/utilities/variables.scss'
+
